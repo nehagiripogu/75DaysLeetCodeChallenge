e1a370b9554865d95da2a0fe9726c650f3fb2584
Giripogu Neha <108928963+nehagiripogu@users.noreply.github.com>
2026-04-13 11:59:01 +0530

    Time: 28 ms (6.51%), Space: 12.5 MB (29%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        
+        original = x
+        rev = 0
+        
+        while x > 0:
+            digit = x % 10
+            rev = rev * 10 + digit
+            x //= 10
+        
+        return original == rev
