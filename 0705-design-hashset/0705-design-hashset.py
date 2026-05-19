3e05236d202fddaf3f2bb4ea7e24b4f81957edbf
Giripogu Neha <108928963+nehagiripogu@users.noreply.github.com>
2026-05-19 22:47:11 +0530

    Time: 54 ms (61.89%), Space: 17 MB (78.48%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,25 @@
+class MyHashSet(object):
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def hash(self, key):
+        return key % self.size
+
+    def add(self, key):
+        index = self.hash(key)
+
+        if key not in self.buckets[index]:
+            self.buckets[index].append(key)
+
+    def remove(self, key):
+        index = self.hash(key)
+
+        if key in self.buckets[index]:
+            self.buckets[index].remove(key)
+
+    def contains(self, key):
+        index = self.hash(key)
+
+        return key in self.buckets[index]
